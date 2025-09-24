b3b6842d566a3f5c451827bfd768b22479dddb0b
Alex <alvi363@vidalibarraquer.net>
2025-09-24 16:42:46 +0200

    Pt2

---
+++ Pt2_Viclhez_Alexandre/settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Pt2_Viclhez_Alexandre"
+include(":app")
+ 
